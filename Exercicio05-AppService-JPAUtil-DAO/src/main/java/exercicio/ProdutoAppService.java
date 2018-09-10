2f1e44583cfc6bb19e37651bcb847f55c91f5b14
Aluno <Aluno@L304-6C.lab.local>
2018-09-10 12:38:06 -0300

    Aula05

--- Exercicio05-AppService-JPAUtil-DAO/src/main/java/exercicio/ProdutoAppService.java
+++ Exercicio05-AppService-JPAUtil-DAO/src/main/java/exercicio/ProdutoAppService.java
@@ -2,126 +2,99 @@ package exercicio;
 
 import java.util.List;
 
-public class ProdutoAppService
-{	
+public class ProdutoAppService {
 	private static ProdutoDAO produtoDAO = FabricaDeDAOs.getDAO(ProdutoDAO.class);
 
-	public long inclui(Produto umProduto) 
-	{	try
-		{	
+	public long inclui(Produto umProduto) {
+		try {
 			// NENHUMA VALIDAÇÃO ESTÁ SENDO REALIZADA AQUI!!!
-		
-==>
+
+			JPAUtil.beginTransaction();
 
 			long numero = produtoDAO.inclui(umProduto);
 
-==>
+			JPAUtil.commitTransaction();
 
 			return numero;
-		} 
-		catch(InfraestruturaException e)
-		{	try
-			{
-==>
-				
+		} catch (InfraestruturaException e) {
+			try {
+				JPAUtil.rollbackTransaction();
+
+			} catch (InfraestruturaException ie) {
 			}
-			catch(InfraestruturaException ie)
-			{				
-			}
-			
-		    throw e;
-		}
-		finally
-		{
-==>
-			
+
+			throw e;
+		} finally {
+			JPAUtil.closeEntityManager();
+
 		}
 	}
 
-	public void altera(Produto umProduto)
-		throws ProdutoNaoEncontradoException
-	{	try
-		{	JPAUtil.beginTransaction();
+	public void altera(Produto umProduto) throws ProdutoNaoEncontradoException {
+		try {
+			JPAUtil.beginTransaction();
 
 			produtoDAO.altera(umProduto);
 
 			JPAUtil.commitTransaction();
-		} 
-		catch(ObjetoNaoEncontradoException e)
-		{	
+		} catch (ObjetoNaoEncontradoException e) {
 			JPAUtil.rollbackTransaction();
 
 			throw new ProdutoNaoEncontradoException("Produto não encontrado");
-		}
-		catch(InfraestruturaException e)
-		{	try
-			{	JPAUtil.rollbackTransaction();
-			}
-			catch(InfraestruturaException ie)
-			{				
+		} catch (InfraestruturaException e) {
+			try {
+				JPAUtil.rollbackTransaction();
+			} catch (InfraestruturaException ie) {
 			}
 
-		    throw e;
-		}
-		finally
-		{   JPAUtil.closeEntityManager();
+			throw e;
+		} finally {
+			JPAUtil.closeEntityManager();
 		}
 	}
-	
-	public void exclui(long numero) 
-		throws ProdutoNaoEncontradoException
-	{	try
-		{	JPAUtil.beginTransaction();
+
+	public void exclui(long numero) throws ProdutoNaoEncontradoException {
+		try {
+			JPAUtil.beginTransaction();
 
 			produtoDAO.exclui(numero);
 
 			JPAUtil.commitTransaction();
-		} 
-		catch(ObjetoNaoEncontradoException e)
-		{	
+		} catch (ObjetoNaoEncontradoException e) {
 			JPAUtil.rollbackTransaction();
 
-		    throw new ProdutoNaoEncontradoException("Produto não encontrado");
-		}
-		catch(InfraestruturaException e)
-		{	try
-			{	JPAUtil.rollbackTransaction();
-			}
-			catch(InfraestruturaException ie)
-			{				
+			throw new ProdutoNaoEncontradoException("Produto não encontrado");
+		} catch (InfraestruturaException e) {
+			try {
+				JPAUtil.rollbackTransaction();
+			} catch (InfraestruturaException ie) {
 			}
 
-		    throw e;
-		}
-		finally
-		{   JPAUtil.closeEntityManager();
+			throw e;
+		} finally {
+			JPAUtil.closeEntityManager();
 		}
 	}
 
-	public Produto recuperaUmProduto(long numero) 
-		throws ProdutoNaoEncontradoException
-	{	
-		try
-		{	Produto umProduto = produtoDAO.recuperaUmProduto(numero);
-			
+	public Produto recuperaUmProduto(long numero) throws ProdutoNaoEncontradoException {
+		try {
+			Produto umProduto = produtoDAO.recuperaUmProduto(numero);
+
 			return umProduto;
-		} 
-		catch(ObjetoNaoEncontradoException e)
-		{	throw new ProdutoNaoEncontradoException("Produto não encontrado");
-		}
-		finally
-		{   JPAUtil.closeEntityManager();
+		} catch (ObjetoNaoEncontradoException e) {
+			throw new ProdutoNaoEncontradoException("Produto não encontrado");
+		} finally {
+			JPAUtil.closeEntityManager();
 		}
 	}
 
-	public List<Produto> recuperaProdutos() 
-	{	try
-		{	List<Produto> produtos = produtoDAO.recuperaProdutos();
+	public List<Produto> recuperaProdutos() {
+		try {
+			List<Produto> produtos = produtoDAO.recuperaProdutos();
 
 			return produtos;
-		} 
-		finally
-		{   JPAUtil.closeEntityManager();
+		} finally {
+			JPAUtil.closeEntityManager();
 		}
 	}
 }
